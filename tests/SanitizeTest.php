e0d68a04c6fecdb939d61189273f221e329fbc7f
James McQuillan <james@pdyn.net>
2016-02-06 03:17:57 -0500

    Add old db_ver tests for future incorporation

--- tests/SanitizeTest.php
+++ tests/SanitizeTest.php
@@ -21,4 +21,41 @@ class SanitizeTest extends \PHPUnit_Framework_TestCase {
 			$this->assertEquals($expected, $actual);
 		}
 	}
+
+	/**
+	 * Dataprovider for test_set_db_ver().
+	 *
+	 * @return array Array of arrays of test parameters.
+	 */
+	/*
+	public function dataprovider_test_set_db_ver() {
+		return [
+			[true, false, '1.5'],
+			[false, false, '1.5'],
+			[null, false, '1.5'],
+			['', false, '1.5'],
+			[[], false, '1.5'],
+			['1.2.3!@#$%^&*()+={}[]:;\'"<>,.?/`~4test', true, '1.2.3.4test'],
+			['1.2test', true, '1.2test'],
+			['2beta', true, '2beta'],
+		];
+	}
+	*/
+
+	/**
+	 * Tests setting and getting the database version.
+	 *
+	 * Tests $DB->set_db_ver() and $DB->get_db_ver();
+	 *
+	 * @dataProvider dataprovider_test_set_db_ver
+	 */
+	/*
+	public function test_set_db_ver($new_version, $expected_return, $expected_version) {
+		return false;
+		$actual_return = $this->DB->set_db_ver($new_version);
+		$actual_version = $this->DB->get_db_ver();
+		$this->assertEquals($expected_return, $actual_return);
+		$this->assertEquals($expected_version, $actual_version);
+	}
+	*/
 }
